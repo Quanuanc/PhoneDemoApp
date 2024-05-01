876c9771064ed71f7743a44411af407e3a56f175
Quanuanc <quanuanc@outlook.com>
2024-05-01 23:05:03 +0800

    Initial Commit

---
+++ PhoneDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhoneDemoApp
+//
+//  Created by Cheng Chuan on 2024/5/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhoneDemoApp/PhoneDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneDemoAppApp.swift
+//  PhoneDemoApp
+//
+//  Created by Cheng Chuan on 2024/5/1.
+//
+
+import SwiftUI
+
+@main
+struct PhoneDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
